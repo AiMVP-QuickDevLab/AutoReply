11eee4e3d62b37de30c4cd80fc88d9dde3a45d3b
QuickDevLab <khizerpasha1011@gmail.com>
2025-07-30 13:12:42 +0530

    Remove demo video section

---
+++ src/pages/Index.tsx
@@ -0,0 +1,17 @@
+@@ .. @@
+ import { Hero } from "@/components/Hero";
+ import { Navigation } from "@/components/Navigation";
+ import { HowItWorks } from "@/components/HowItWorks";
+ import { Features } from "@/components/Features";
+-import { Demo } from "@/components/Demo";
+ import { Testimonials } from "@/components/Testimonials";
+ import { Stats } from "@/components/Stats";
+ import { Pricing } from "@/components/Pricing";
+@@ .. @@
+         <SocialProof />
+         <HowItWorks />
+         <Features />
+-        <Demo />
+         <Testimonials />
+         <Stats />
+         <Pricing />
